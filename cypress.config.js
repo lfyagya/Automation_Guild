2b999699b68a56bdeb326fe4d1d1f4560584b4d1
unknown <yagyabhatta@lftechnology.com>
2023-07-19 13:45:40 +0545

    Initial Commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,33 +1,7 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  "reporter": "cypress-multi-reporters",
-  "reporterOptions": {
-    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
-    mochaJunitReporterReporterOptions: {
-      mochaFile: 'cypress/reports/junit/results-[hash].xml',
-    },
-    cypressMochawesomeReporterReporterOptions: {
-        reportDir: "cypress/results/json",
-        overwrite: false,
-        html: true,
-        json: true,
-        inlineAssets: true,
-        charts: true,
-        reportPageTitle: 'custom-title',
-        embeddedScreenshots: true,
-        inlineAssets: true,
-        saveAllAttempts: false,
-    }
-  },
-  defaultCommandTimeout: 6000,
+module.exports = {
   e2e: {
-    specPattern: 'cypress/integration/Tests/*.js',
-    screenshotsFolder: 'cypress/screenshots',
-    screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
-});
+};
--- cypress/plugin/index.js
+++
@@ -1,6 +0,0 @@
-/// <reference types="cypress" />
-
-
-module.exports = (on, config) => {
-    require('cypress-mochawesome-reporter/plugin')(on);
-  };
